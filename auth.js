8e6f7f0c1219575c932bbcf4b909b352b85d6731
Steve Tyler <mail@steve-tyler.co.uk>
2015-06-06 13:23:37 +0100

    added twitter access key and token

---
+++ auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'twitterAuth' : {
+		'consumerKey': 'XyvBxHeu6CQp0WXyo8zMngtQn',
+		'consumerSecret': 'Pq92CeMt3Cnx6SWrgOWG7Pk580eaHhZoDMCmdFOdPZwbqvfsF8',
+		'callbackURL': 'http://www.webdevfavs.com/api/users/auth/twitter/callback'
+	}
+};
