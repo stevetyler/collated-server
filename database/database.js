9c2a5c3ac40b475d931fbb925a142dc8f0c3bc2e
Steve Tyler <mail@steve-tyler.co.uk>
2015-07-13 23:09:53 +0100

    added tag schema

--- database/database.js
+++ database/database.js
@@ -4,6 +4,7 @@ logger.info('load database.js');
 var mongoose = require('mongoose');
 var userSchema = require('../schemas/user');
 var favSchema = require('../schemas/fav');
+var tagSchema = require('../schemas/tag');
 
 // TTL errors when creating new user :
 // http://stackoverflow.com/questions/22698661/mongodb-error-setting-ttl-index-on-collection-sessions
@@ -11,7 +12,7 @@ mongoose.connect('mongodb://localhost/webdevfavs'); // pending, then emits 'open
 
 mongoose.connection.model('User', userSchema);
 mongoose.connection.model('Fav', favSchema);
-
+mongoose.connection.model('Tag', tagSchema);
 
 
 module.exports = mongoose.connection;
