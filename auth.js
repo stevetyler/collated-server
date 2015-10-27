3ece561c1956720b94b8091c2f2400ab71fee500
Steve Tyler <mail@steve-tyler.co.uk>
2015-10-27 21:35:51 +0000

    changed redirect url

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ module.exports = {
 	'twitterAuth' : {
 		'consumerKey': 'XyvBxHeu6CQp0WXyo8zMngtQn',
 		'consumerSecret': 'Pq92CeMt3Cnx6SWrgOWG7Pk580eaHhZoDMCmdFOdPZwbqvfsF8',
-		'callbackURL': 'http://www.webdevfavs.com/api/users/auth/twitter/callback'
+		'callbackURL': 'http://www.collated.net/api/users/auth/twitter/callback'
 	}
 };
 
