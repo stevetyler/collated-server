4f5a45339b4e11a524d06f761f3d796cd432478f
Steve Tyler <stevetyler.uk@gmail.com>
2015-12-22 18:32:43 +0000

    updated callback urls

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ if(process.env.NODE_ENV === 'production') {
 		'twitterAuth' : {
 			'consumerKey': 'XyvBxHeu6CQp0WXyo8zMngtQn',
 			'consumerSecret': 'Pq92CeMt3Cnx6SWrgOWG7Pk580eaHhZoDMCmdFOdPZwbqvfsF8',
-			'callbackURL': 'http://www.collated.net/api/users/auth/twitter/callback'
+			'callbackURL': 'http://collated.net/api/users/auth/twitter/callback'
 		}
 	};
 } else {
@@ -13,7 +13,7 @@ if(process.env.NODE_ENV === 'production') {
 		'twitterAuth' : {
 			'consumerKey': 'LQeZCC9ekkkwJpJiXoIzJ0pbC',
 			'consumerSecret': 'm9bITFxUC0g6ZtSZSW1GpV5I5I6hAMctKt7IUWD7WbJqKV7sja',
-			'callbackURL': 'http://localhost:3000/api/users/auth/twitter/callback'
+			'callbackURL': 'http://collated-dev.net/api/users/auth/twitter/callback'
 		}
 	};
 }
