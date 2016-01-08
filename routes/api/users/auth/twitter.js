d7283adf16c0c816c02178e924b1d76c942d2733
Steve Tyler <stevetyler.uk@gmail.com>
2016-01-08 15:03:27 +0000

    updated redirect url

--- routes/api/users/auth/twitter.js
+++ routes/api/users/auth/twitter.js
@@ -3,7 +3,6 @@ var passport = require('../../../../passport/passport-authenticate');
 module.exports.autoroute = {
 	get: {
 		'/twitter' : passport.authenticate('twitter'),
-    	'/twitter/callback' : passport.authenticate('twitter', { successRedirect: 'http://collated-dev.net/with-account', failureRedirect: '/'}),
+    	'/twitter/callback' : passport.authenticate('twitter', { successRedirect: 'http://collated.net/with-account', failureRedirect: '/'}),
 	}
-}
- 
+};
