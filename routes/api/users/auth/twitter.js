2da5b3284391a0aaf7d5711f328b928273071846
Steve Tyler <stevetyler.uk@gmail.com>
2016-01-18 12:07:20 +0000

    added node_env to use different credentials for production

--- routes/api/users/auth/twitter.js
+++ routes/api/users/auth/twitter.js
@@ -1,8 +1,18 @@
 
 var passport = require('../../../../passport/passport-authenticate');
-module.exports.autoroute = {
-	get: {
-		'/twitter' : passport.authenticate('twitter'),
-    	'/twitter/callback' : passport.authenticate('twitter', { successRedirect: 'http://collated.net/with-account', failureRedirect: '/'}),
-	}
-};
+
+if(process.env.NODE_ENV === 'production') {
+	module.exports.autoroute = {
+		get: {
+			'/twitter' : passport.authenticate('twitter'),
+	    	'/twitter/callback' : passport.authenticate('twitter', { successRedirect: 'http://collated.net/with-account', failureRedirect: '/'}),
+		}
+	};
+} else {
+	module.exports.autoroute = {
+		get: {
+			'/twitter' : passport.authenticate('twitter'),
+	    	'/twitter/callback' : passport.authenticate('twitter', { successRedirect: 'http://collated-dev.net/with-account', failureRedirect: '/'}),
+		}
+	};
+}
