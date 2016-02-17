dd993051ad90cd60815310055a9d9a52ef857e7c
Steve Tyler <stevetyler.uk@gmail.com>
2016-02-17 14:51:46 +0000

    updated url to www

--- routes/api/users/auth/twitter.js
+++ routes/api/users/auth/twitter.js
@@ -5,14 +5,14 @@ if(process.env.NODE_ENV === 'production') {
 	module.exports.autoroute = {
 		get: {
 			'/twitter' : passport.authenticate('twitter'),
-	    	'/twitter/callback' : passport.authenticate('twitter', { successRedirect: 'http://collated.net/with-account', failureRedirect: '/'}),
+	    	'/twitter/callback' : passport.authenticate('twitter', { successRedirect: 'http://www.collated.net/with-account', failureRedirect: '/'}),
 		}
 	};
 } else {
 	module.exports.autoroute = {
 		get: {
 			'/twitter' : passport.authenticate('twitter'),
-	    	'/twitter/callback' : passport.authenticate('twitter', { successRedirect: 'http://collated-dev.net/with-account', failureRedirect: '/'}),
+	    	'/twitter/callback' : passport.authenticate('twitter', { successRedirect: 'http://www.collated-dev.net/with-account', failureRedirect: '/'}),
 		}
 	};
 }
