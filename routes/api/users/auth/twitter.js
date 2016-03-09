474262fbeaebebf7f4f6307eae88e27154af933a
Steve Tyler <stevetyler.uk@gmail.com>
2016-03-09 10:43:53 +0000

    updated Twitter callbacks to use https

--- routes/api/users/auth/twitter.js
+++ routes/api/users/auth/twitter.js
@@ -5,7 +5,7 @@ if(process.env.NODE_ENV === 'production') {
 	module.exports.autoroute = {
 		get: {
 			'/twitter' : passport.authenticate('twitter'),
-	    	'/twitter/callback' : passport.authenticate('twitter', { successRedirect: 'http://www.collated.net/with-account', failureRedirect: '/'}),
+	    	'/twitter/callback' : passport.authenticate('twitter', { successRedirect: 'https://www.collated.net/with-account', failureRedirect: '/'}),
 		}
 	};
 } else {
