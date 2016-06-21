09a3f513989ec342bdab56c3494444d8f079ef16
Steve Tyler <mail@steve-tyler.co.uk>
2016-06-21 23:24:14 +0100

    installed morgan and configured logging

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var db = require('./database/database');
 var express = require('express');
+var morgan  = require('morgan');
+var fs = require('fs');
+var path = require('path');
 var autoroute = require('express-autoroute');
+
 var app = express();
-//
+
 // // var CronJob = require('cron').CronJob;
 // // var job = new CronJob('*/1 * * * * ', function() {
 // //   console.log('cron alert');
@@ -18,11 +22,19 @@ require('./express-config')(app);
 
 autoroute(app, {});
 
+// create write stream in append mode
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
+
+// setup logging
+app.use(morgan('combined', {stream: accessLogStream}));
+
+
 // error handling
 app.use(function(err, req, res, next) {
     if (err) throw err;
     res.status(err.status || 500);
 });
+
 
 // http://blog.mongolab.com/2013/11/deep-dive-into-connection-pooling/
 // waiting for 'open' event from mongoose.connection
