1f909d875775f2e4080c158b03699fe6aad22b4b
Steve Tyler <mail@steve-tyler.co.uk>
2016-06-27 13:26:02 +0100

    updated item schema with type property and created makeEmberItem method

--- schemas/item.js
+++ schemas/item.js
@@ -8,12 +8,24 @@ var itemSchema = new Schema({
   author: String,
   createdDate: Date,
   body: String,
-  type: String, // Slack, Tweet etc
+  type: String, // Slack, Tweet, Bookmark
   twitterTweetId: String,
   isPrivate: String
 });
 
-itemSchema.methods.makePrivate = function() {
+// not in use
+itemSchema.methods.makeEmberItem = function() {
+  var emberItem = {
+    id: this._id,
+    user: this.user.id,
+    body: this.body,
+    createdDate: this.createdDate,
+    author: this.author,
+    tags: this.tags,
+    type: this.type,
+    isPrivate: this.isPrivate
+  };
+  return emberItem;
 };
 
 module.exports = itemSchema;
