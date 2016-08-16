987c5493aa8c032f0932eeb5bde342f9cc36f46d
Steve Tyler <mail@steve-tyler.co.uk>
2016-08-16 09:45:04 +0100

    refactored tags route with ES6 syntax

--- routes/api/v1/tags.js
+++ routes/api/v1/tags.js
@@ -1,10 +1,8 @@
-var async = require('async');
+const db = require('../../../database/database');
+const ensureAuthenticated = require('../../../middlewares/ensure-authenticated').ensureAuthenticated;
 
-var db = require('../../../database/database');
-var ensureAuthenticated = require('../../../middlewares/ensure-authenticated').ensureAuthenticated;
-
-var Item = db.model('Item');
-var Tag = db.model('Tag');
+const Item = db.model('Item');
+const Tag = db.model('Tag');
 
 module.exports.autoroute = {
 	get: {'/tags' : getTags},
@@ -14,8 +12,8 @@ module.exports.autoroute = {
 };
 
 function getTags(req, res){
-	if (req.query.operation === 'userTags') {getUserTags(req, res);}
-	if (req.query.operation === 'slackTeamTags') {getSlackTeamTags(req, res);}}
+	if (req.query.operation === 'userTags') { getUserTags(req, res); }
+	if (req.query.operation === 'slackTeamTags') { getSlackTeamTags(req, res);} }
 
 // function createDefaultTags(id) {
 // 	return Tag.findOne({id: 'undefined', user: id}).exec().then(function(tag) {
@@ -31,14 +29,12 @@ function getTags(req, res){
 // }
 
 function getUserTags(req, res) {
-	var id = req.query.userId;
-	// var allEmberTags = [];
-	// var publicEmberTags = [];
+	const id = req.query.userId;
 
 	if (!id) {
 		return res.status(404).end();
 	}
-	Tag.findOne({id: 'undefined', user: id}).exec().then(function(tag) {
+	Tag.findOne({id: 'undefined', user: id}).exec().then((tag) => {
 		if (!tag) {
 			Tag.create({
 				id: 'undefined',
@@ -48,15 +44,15 @@ function getUserTags(req, res) {
 			});
 		}
 	})
-	.then(function() {
+	.then(() => {
 		return Tag.find({user: id});
 	})
-	.then(function(tags) {
+	.then((tags) => {
 		if (tags) {
-			return makeEmberTags(id, tags);
+			return makeEmberTags(id, tags, 'user');
 		}
 	})
-	.then(function(obj) {
+	.then((obj) => {
 	  if (!req.user) {
 			return obj.public;
 	  }
@@ -67,19 +63,26 @@ function getUserTags(req, res) {
 	    return obj.public;
 	  }
 	})
-	.then(function(tags) {
+	.then((tags) => {
 		res.send({ tags: tags });
-	}, function() {
+	}, () => {
 		return res.status(404).end();
 	});
 }
 
-function makeEmberTags(id, tags) {
-	var tagPromises = tags.map(tag => Item.count({ user: id, tags: { $in: [ tag.id ] }}));
+function makeEmberTags(id, tags, type) {
+	let tagPromises;
+
+	if (type === 'user') {
+		tagPromises = tags.map(tag => Item.count({ user: id, tags: { $in: [ tag.id ] }}));
+	}
+	else if (type === 'slack') {
+		tagPromises = tags.map(tag => Item.count({ slackTeamId: id, tags: {$in: [tag.id] }}));
+	}
 
 	return Promise.all(tagPromises).then(counts => {
 		return tags.reduce((obj, tag, i) => {
-			var emberTag = tag.makeEmberTag(counts[i]);
+			const emberTag = tag.makeEmberTag(counts[i]);
 			return tag.isPrivate === 'true' ?
 				{
 					all: obj.all.concat(emberTag),
@@ -91,80 +94,46 @@ function makeEmberTags(id, tags) {
 		}, { all: [], public: [] });
 	});
 
-	//var a = [ 1, 2, 3 ]
-	//var b = [ 0, ...a, 4, ...[ 5 ] ]
+	//const a = [ 1, 2, 3 ]
+	//const b = [ 0, ...a, 4, ...[ 5 ] ]
 }
 
 function getSlackTeamTags(req, res) {
-	var teamId = req.query.teamId;
-	var allEmberTags = [];
-	var publicEmberTags = [];
+	const teamId = req.query.teamId;
 
 	if (!teamId) {
 		return res.status(404).end();
 	}
-	Tag.find({slackTeamId: teamId}).exec().then(function(tags) {
+	Tag.find({slackTeamId: teamId}).exec().then((tags) => {
 		if (tags) {
-			makeEmberSlackTags(req, res, teamId, allEmberTags, publicEmberTags, tags);
+			return makeEmberTags(teamId, tags, 'slack');
 		}
 	})
-	.then(null, function(err) {
-		console.log(err);
+	.then((obj) => {
+		res.send({ tags: obj.all });
+	}, () => {
 		return res.status(404).end();
 	});
 }
 
-function makeEmberSlackTags(req, res, teamId, allEmberTags, publicEmberTags, tags) {
-  async.each(tags, function(tag, done) {
-    Item.count({slackTeamId: teamId, tags: {$in: [tag.id]}}, function(err, count) {
-      if (err) {
-        return res.status(500).end();
-      }
-      var emberTag = tag.makeEmberTag(count);
-
-      if (tag.isPrivate === 'true') {
-        allEmberTags.push(emberTag);
-      }
-			else {
-        allEmberTags.push(emberTag);
-        publicEmberTags.push(emberTag);
-      }
-      done();
-    });
-  }, function(err) {
-    if (err) {
-      console.log(err);
-    }
-    if (!req.user) {
-      return res.send({'tags': publicEmberTags});
-    }
-    else if (req.user.id === req.query.userId) {
-      return res.send({'tags': allEmberTags});
-    }
-    else {
-      return res.send({'tags': publicEmberTags});
-    }
-  });
-}
-
 function postTag(req, res){
 	if (req.user.id === req.body.tag.user) {
-		var tag = {
-			id: req.body.tag.id,
-			colour: req.body.tag.colour,
-			user: req.body.tag.user,
-			isPrivate: req.body.tag.isPrivate,
-			slackChannelId: req.body.tag.slackChannelId,
-			slackTeamId: req.body.tag.slackTeamId
-		};
 		if (req.body.tag.id) {
-			Tag.findOne({id: req.body.tag.id, user: req.body.tag.user}, function(err, data) {
+			const tag = {
+				id: req.body.tag.id,
+				colour: req.body.tag.colour,
+				user: req.body.tag.user,
+				isPrivate: req.body.tag.isPrivate,
+				slackChannelId: req.body.tag.slackChannelId,
+				slackTeamId: req.body.tag.slackTeamId
+			};
+			Tag.findOne({id: req.body.tag.id, user: req.body.tag.user}, (err, data) => {
 				if (data) {
 					res.status(400).end();
 				}
 				else {
-					var newTag = new Tag(tag);
-					newTag.save(function(err, tag) {
+					const newTag = new Tag(tag);
+					newTag.save((err, tag) => {
 						if (err) {
 							res.status(501).end();
 						}
@@ -180,8 +149,8 @@ function postTag(req, res){
 }
 
 function putTag(req, res) {
-  var tagId = req.params.id;
-  var isPrivate = req.body.tag.isPrivate;
+  const tagId = req.params.id;
+  const isPrivate = req.body.tag.isPrivate;
 
   if (req.user.id === req.body.tag.user) {
     Tag.update({id: tagId, user: req.user.id},
@@ -191,21 +160,21 @@ function putTag(req, res) {
         isPrivate: req.body.tag.isPrivate
         }
       }
-    ).exec().then(function() {
-      Item.find({user: req.user.id, tags: {$in: [tagId]}}, function(err, items) {
+    ).exec().then(() => {
+      Item.find({user: req.user.id, tags: {$in: [tagId]}}, (err, items) => {
         if (err) {
           return res.status(404).send();
         }
-        items.forEach(function(item) {
+        items.forEach((item) => {
           item.isPrivate = isPrivate;
           return item.save();
         });
       });
     })
-    .then(function() {
+    .then(() => {
       return res.send({});
     })
-    .then(null, function(err) {
+    .then(null, (err) => {
       console.log(err);
       return res.status(400).end();
     });
@@ -216,10 +185,10 @@ function putTag(req, res) {
 }
 
 function deleteTag(req, res){
-  Tag.remove({ id: req.params.id }).exec().then(function() {
+  Tag.remove({ id: req.params.id }).exec().then(() => {
     return res.send({});
   })
-	.then(null, function(err) {
+	.then(null, (err) => {
 		console.log(err);
 		return res.status(500).end();
 	});
