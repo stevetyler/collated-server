1ecf06799402a653c72eda4c88aeb44d96652ec9
Steve Tyler <mail@steve-tyler.co.uk>
2016-10-26 17:29:27 +0100

    renamed saveOneTweet to saveTweetItem

--- routes/api/v1/items.js
+++ routes/api/v1/items.js
@@ -454,33 +454,6 @@ function saveChromeItem(reqBody) {
 	});
 }
 
-// function assignTagsToItem(titleText, id, type) {
-//   var text = titleText.toLowerCase();
-// 	console.log('text', text);
-// 	var tagQuery = (type === 'slack' ? {slackTeamId: id} : {user: id});
-//
-// 	return Tag.find(tagQuery).then(tags => {
-// 		return tags.reduce((obj, tag) => {
-// 			let tagname = tag.name.toLowerCase();
-//
-// 	    if (text.indexOf(tagname) !== -1) {
-// 	      console.log('tag found', tag);
-// 	      obj.tagIds.push(tag._id);
-// 	    }
-// 			if (tagname === 'unassigned') {
-// 				obj.unassignedId.push(tag._id);
-// 			}
-// 			return obj;
-// 	  }, {tagIds: [], unassignedId: []});
-// 	}).then(tagsObj => {
-// 		console.log('tags returned', tagsObj);
-// 		if (tagsObj.tagIds.length > 0) {
-// 			return tagsObj.tagIds;
-// 		}
-// 		return tagsObj.unassignedId;
-// 	});
-// }
-
 function containsUrl(message) {
 	return /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig.test(message);
 }
