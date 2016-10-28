636e6f110629af74847a6bbd0b5b25114c5754b4
Steve Tyler <mail@steve-tyler.co.uk>
2016-10-28 17:22:03 +0100

    renamed route names and updated userGroups route

--- routes/api/v1/userGroups.js
+++ routes/api/v1/userGroups.js
@@ -5,15 +5,16 @@ const UserGroup = db.model('UserGroup');
 
 module.exports.autoroute = {
 	get: {
-		'/user-groups/:id': getUserGroupHandler
+		'/userGroups/:id': getUserGroupHandler
 	}
 };
 
 function getUserGroupHandler(req, res) {
-  const queryId = req.query.id;
+  const queryId = formatGroupId(req.params.id);
 	const authUser = req.user;
 	const resObj = {};
 
+	console.log('userGroupHandler called', queryId, authUser);
 	getOrCreateUserGroup(queryId, authUser).then(userGroup => {
 		const emberUserGroup = userGroup.makeEmberUserGroup();
 
@@ -35,14 +36,21 @@ function getUserGroupHandler(req, res) {
 function getOrCreateUserGroup(queryId, authUser) {
   return UserGroup.findOne({id: queryId}).then(userGroup => {
     if (!userGroup) {
-      let newUserGroup = {
-				id: authUser.slackProfile.teamDomain,
+			let newId = formatGroupId(authUser.slackProfile.teamDomain);
+			let newUserGroup = new UserGroup({
+				id: newId,
         categoriesEnabled: false,
         slackTeamId: authUser.slackProfile.teamId,
         slackTeamDomain: authUser.slackProfile.teamDomain,
-      };
-			return UserGroup.create(newUserGroup);
+      });
+			return newUserGroup.save().then(group => {
+				console.log('group created', group);
+			});
     }
-		return userGroup;
+		//return userGroup;
   });
+}
+
+function formatGroupId(name) {
+	return name.split(' ').join('-');
 }
