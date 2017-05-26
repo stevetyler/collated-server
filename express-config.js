29c53b626d775a32c3cd93626bcfe76c353ab2ee
Steve Tyler <mail@steve-tyler.co.uk>
2017-05-26 17:45:39 +0100

    changed session cookie expiry to 1 week

--- express-config.js
+++ express-config.js
@@ -2,6 +2,7 @@
 // http://expressjs.com/guide/using-middleware.html#middleware.application
 //var cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 const session = require('express-session');
 
@@ -11,11 +12,14 @@ const MongoStore = require('connect-mongostore')(session);
 const mongoose = require('mongoose');
 
 module.exports = function (app) {
+	app.use(cookieParser());
 	app.use(session({
+		cookie: {
+			maxAge: 8.64 * Math.pow(10, 7) // 1 week
+		},
 		secret: auth.session.secret,
 		resave: true,  // true forces session to be saved even when unmodified. Not needed for Mongo
 		saveUninitialized: false, // true forces a new unmodified session to be saved to the store. Passport will always modify
-		//store: new MongoStore({ mongooseConnection: mongoose.connection, auto_reconnect: true })
 		store: new MongoStore({
 			mongooseConnection: mongoose.connection,
 			auto_reconnect: true
