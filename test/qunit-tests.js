511366bfc7f94576c75ca0960aca0453b71f9255
Steve Tyler <mail@steve-tyler.co.uk>
2017-06-24 18:20:13 +0100

    setup qunit server

---
+++ test/qunit-tests.js
@@ -0,0 +1,7 @@
+
+
+QUnit.test("a basic test example", function (assert) {
+    assert.ok(true, "this test is fine");
+    var value = "hello";
+    assert.equal("hello", value, "We expect value to be hello");
+});
