dbcc779cb535003d734cf04e0694fbd0cfb5221e
Steve Tyler <mail@steve-tyler.co.uk>
2017-07-13 22:50:40 +0100

    installed express-validator and updated express config

--- express-config.js
+++ express-config.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 const session = require('express-session');
+const expressValidator = require('express-validator');
 
 const auth = require('./auth');
 const passport = require('./passport/passport-authenticate');
@@ -30,6 +31,9 @@ module.exports = function (app) {
 
 	app.use(bodyParser.json({limit: '50mb'}));
 	app.use(bodyParser.urlencoded({limit: '50mb', extended: true})); // parse application/x-www-form-urlencoded
+	app.use(expressValidator({
+		
+	})); // this line must be immediately after any of the bodyParser middlewares!
 	app.use(fileUpload());
 	app.use(passport.initialize());
 	app.use(passport.session());
