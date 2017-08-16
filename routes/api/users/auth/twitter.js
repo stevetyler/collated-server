350c7f3c2a1cfb2f4fa2eef59f5d6a4b8816722e
Steve Tyler <mail@steve-tyler.co.uk>
2017-08-16 19:04:52 +0100

    started to refactor auth twitter route

--- routes/api/users/auth/twitter.js
+++ routes/api/users/auth/twitter.js
@@ -1,18 +1,50 @@
+'use strict';
 
-var passport = require('../../../../passport/passport-authenticate');
+let passport = require('../../../../passport/passport-authenticate');
+//let isIos;
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
 	module.exports.autoroute = {
 		get: {
 			'/twitter' : passport.authenticate('twitter'),
-	    	'/twitter/callback' : passport.authenticate('twitter', { successRedirect: 'https://app.collated.net/with-account', failureRedirect: '/'}),
+	    '/twitter/callback' : passport.authenticate('twitter', {
+				successRedirect: 'https://app.collated.net/with-account',
+				failureRedirect: '/'
+			}),
 		}
 	};
 } else {
 	module.exports.autoroute = {
 		get: {
 			'/twitter' : passport.authenticate('twitter'),
-	    	'/twitter/callback' : passport.authenticate('twitter', { successRedirect: 'http://www.collated-dev.net/with-account', failureRedirect: '/'}),
+	    '/twitter/callback' : [
+				//getQueryParam,
+				passport.authenticate('twitter', {
+					//successRedirect: isIos ? 'net.collated.ios://' : 'http://www.collated-dev.net/with-account', // isIos always false
+					failureRedirect: '/'
+				}),
+				function(req, res) {
+					console.log('query', req.query, 'req.user', req.user);
+					//console.log('get isIos ', isIos);
+					if (req.user.ios) {
+						res.redirect('net.collated.ios://');
+					}
+					else {
+						res.redirect('http://www.collated-dev.net/with-account');
+					}
+				}
+			]
 		}
 	};
+}
+
+
+function getQueryParam(req, res, next) {
+	console.log('get queryParam iOS ', req.query.ios);
+
+	if (req.query.ios === 'true') {
+		req.user.ios = true;
+	}
+	//console.log('get query iOS ', isIos);
+	next();
 }
