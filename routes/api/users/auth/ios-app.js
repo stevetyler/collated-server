c9cbfde0d38d3401dd6fda6f8195649f375738c1
Steve Tyler <mail@steve-tyler.co.uk>
2017-08-29 21:37:15 +0100

    refactored ios auth to store passport sid instead of api token

--- routes/api/users/auth/ios-app.js
+++ routes/api/users/auth/ios-app.js
@@ -1,14 +1,21 @@
 'use strict';
-const passport = require('../../../../passport/passport-authenticate');
-const helpers = require('../../../../lib/utilities/helpers');
+//const passport = require('../../../../passport/passport-authenticate');
+//const helpers = require('../../../../lib/utilities/helpers');
 
 module.exports.autoroute = {
   get: {
-    '/ios-app' : [
-      passport.authenticate('custom', {
-        failureRedirect: '/',
-      }),
-      helpers.authSuccessRedirect
-    ]
+    '/ios-app' : iosRedirect
   }
 };
+
+function iosRedirect(req, res) {
+  // set query param as cookie and redirect to with-account
+  let sid = req.query.token;
+  console.log('sid to set');
+
+  res.cookie = res.cookie('connect.sid', sid, {
+    expires: new Date(Date.now() + 6 * Math.pow(10, 8)), // expires in a week
+    httpOnly: true
+  });
+  res.redirect('/with-account');
+}
