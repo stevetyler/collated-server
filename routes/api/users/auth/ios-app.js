cbd985d2a12a9a76160b3defbcd4d1f24943d4b8
Steve Tyler <mail@steve-tyler.co.uk>
2017-08-30 14:48:10 +0100

    added logs to ios-app route

--- routes/api/users/auth/ios-app.js
+++ routes/api/users/auth/ios-app.js
@@ -1,14 +1,23 @@
 'use strict';
 const passport = require('../../../../passport/passport-authenticate');
-const helpers = require('../../../../lib/utilities/helpers');
+//const helpers = require('../../../../lib/utilities/helpers');
 
 module.exports.autoroute = {
   get: {
     '/ios-app' : [
       passport.authenticate('custom', {
-        failureRedirect: '/',
+        failureRedirect: 'net.collated.ios://',
       }),
-      helpers.authSuccessRedirect
+      authSuccessRedirect
     ]
   }
 };
+
+
+function authSuccessRedirect(req, res) {
+  console.log('auth success called');
+  let withAccountPath = process.env.NODE_ENV === 'production' ?
+		'https://app.collated.net/with-account' : 'http://www.collated-dev.net/with-account';
+
+  res.redirect(withAccountPath); // redirecting to Safari not ViewController
+}
