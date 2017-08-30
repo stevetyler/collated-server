c85ad013a252e1e0fc24ff5a98913e4f1aa034b3
Steve Tyler <mail@steve-tyler.co.uk>
2017-08-30 15:58:10 +0100

    updated ios-app route with failureRedirect path for iOS

--- routes/api/users/auth/ios-app.js
+++ routes/api/users/auth/ios-app.js
@@ -1,23 +1,21 @@
 'use strict';
 const passport = require('../../../../passport/passport-authenticate');
-//const helpers = require('../../../../lib/utilities/helpers');
 
 module.exports.autoroute = {
   get: {
     '/ios-app' : [
       passport.authenticate('custom', {
-        failureRedirect: 'net.collated.ios://',
+        failureRedirect: 'net.collated.ios://', // redirects back to iOS app, not sure why it's called?
       }),
       authSuccessRedirect
     ]
   }
 };
 
-
 function authSuccessRedirect(req, res) {
   console.log('auth success called');
   let withAccountPath = process.env.NODE_ENV === 'production' ?
 		'https://app.collated.net/with-account' : 'http://www.collated-dev.net/with-account';
 
-  res.redirect(withAccountPath); // redirecting to Safari not ViewController
+  res.redirect(withAccountPath);
 }
