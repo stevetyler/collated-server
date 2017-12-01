0f0996f35a930ad23d4833edd669f603567afa9d
Steve Tyler <stevetyler.uk@gmail.com>
2017-12-01 13:13:58 +0000

    added new api routes for android

---
+++ routes/api/users/auth/twitter-android.js
@@ -0,0 +1,13 @@
+'use strict';
+const helpers = require('../../../../lib/utilities/helpers');
+
+module.exports.autoroute = {
+  get: {
+    '/twitter-android' : function(req, res) {
+      let options = helpers.authCookieOptions;
+
+      res.cookie('android', 'true', options);
+      res.redirect('/api/users/auth/twitter/callback');
+    }
+  }
+};
