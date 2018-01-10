52a111edc3beabf257d9d11eedacd22c426c7efd
Steve Tyler <mail@steve-tyler.co.uk>
2018-01-10 18:09:15 +0000

    updated helper successRedirect for Android to redirect to continue button

--- routes/api/users/auth/android-app.js
+++ routes/api/users/auth/android-app.js
@@ -17,6 +17,5 @@ function authSuccessRedirect(req, res) {
   let withAccountPath = process.env.NODE_ENV === 'production' ?
 	'https://app.collated.net/with-account' : 'http://www.collated-dev.net/with-account';
 
-  //res.redirect('https://app.collated.net/twitter-android');
   res.redirect(withAccountPath);
 }
