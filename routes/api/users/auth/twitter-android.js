684079d515924c563361e6a600b7481caf508218
Steve Tyler <mail@steve-tyler.co.uk>
2018-01-10 14:31:05 +0000

    added logs for debugging android auth error

--- routes/api/users/auth/twitter-android.js
+++ routes/api/users/auth/twitter-android.js
@@ -7,6 +7,7 @@ module.exports.autoroute = {
       let options = helpers.authCookieOptions;
 
       res.cookie('android', 'true', options);
+      console.log('redirecting to Twitter callback from twitter-android');
       //res.redirect('https://app.collated.net/twitter-android');
       res.redirect('/api/users/auth/twitter/callback');
     }
