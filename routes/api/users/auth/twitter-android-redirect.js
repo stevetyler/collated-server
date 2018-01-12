b7799a7ffd0b02bd64408f5270aa6462666eded1
Steve Tyler <mail@steve-tyler.co.uk>
2018-01-12 17:18:58 +0000

    adding logging to auth/twitter route and added new redirect route for Android

---
+++ routes/api/users/auth/twitter-android-redirect.js
@@ -0,0 +1,16 @@
+'use strict';
+const helpers = require('../../../../lib/utilities/helpers');
+
+module.exports.autoroute = {
+  get: {
+    '/twitter-android-redirect' : function(req, res) {
+      let options = helpers.authCookieOptions;
+
+      res.cookie('android', 'true', options);
+      console.log('redirecting to Twitter callback from twitter-android-redirect');
+      //console.log('redirecting android continue route from twitter-android');
+      //res.redirect('https://app.collated.net/twitter-android');
+      res.redirect('/api/users/auth/twitter/callback');
+    }
+  }
+};
