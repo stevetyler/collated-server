4c203adef452d8addf018ddd0c697461e705d992
Steve Tyler <mail@steve-tyler.co.uk>
2018-01-19 14:27:31 +0000

    updated android auth routes due to social login redirect page still showing after custom scheme redirect

---
+++ routes/api/users/auth/android-success-redirect.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports.autoroute = {
+  get: {
+    '/android-success-redirect' : function(req, res) {
+      try {
+        let token = req.user.apiKeys.collatedToken;
+        let userId = req.user.id;
+
+        console.log('android true for user ', userId, token);
+        res.redirect('net.collated.android://' + token);
+      }
+      catch(err) {
+        console.log(err);
+      }
+    }
+  }
+};
